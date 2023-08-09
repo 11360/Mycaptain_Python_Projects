55cbcda8c4bbbdb94761ab07010ef56ffbdfa43f
11360 <104075716+11360@users.noreply.github.com>
2023-08-09 08:35:39 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> r=float(input("Input the radius of the circle: "))
+Input the radius of the circle: 1.1
+>>> p=math.pi
+>>> a=p*r*r
+>>> print("The area of the circle with radius {0} is {1}".format(r,a))
+The area of the circle with radius 1.1 is 3.8013271108436504
+>>> 
